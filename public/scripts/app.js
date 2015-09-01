c59765f4b6f8aa9f4dc38a66e9ebaff295818088
Alex Chan <alex415@gmail.com>
2015-09-01 15:38:14 -0700

    foursquare request working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,18 +3,14 @@ angular.module('pubCrawl', ['ngAutocomplete', 'ngMap'])
 .controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 
   $scope.submit = function () {
-    var tag = $scope.autocomplete;
-    var url = 'https://api.foursquare.com/v2/venues/categories?client_id=0LQEK2QFONRMHNYOBLU4ZEMSGKGWAB5J51O4JB0DPYRNW41G&client_secret=JYZ2IHWEDEKK5A3HNQKO4ELARI55YOJP0LFOF1NFM3R3LY5Z&v=20150901';
+    var geolocation = $scope.details.geometry.location.toString().replace(/\s+/, '').slice(1, -1);
+    // console.log(geolocation);
+    var url = 'https://api.foursquare.com/v2/venues/search?radius=1000&ll=' + geolocation + '&limit=10&client_id=0LQEK2QFONRMHNYOBLU4ZEMSGKGWAB5J51O4JB0DPYRNW41G&v=20150901&client_secret=JYZ2IHWEDEKK5A3HNQKO4ELARI55YOJP0LFOF1NFM3R3LY5Z&categoryId=4bf58dd8d48988d116941735';
     $http.get(url)
       .then(function (response) {
         console.log(response);
-        console.log("hello");
     });
   };
-
-    $scope.autocomplete = '';
-    $scope.details = '';
-    $scope.options = '';
 
 
 }]); // end
