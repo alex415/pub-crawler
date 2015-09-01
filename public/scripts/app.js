5f75ea4db71850bae4261ce07eb9d88bf5403e92
Alex Chan <alex415@gmail.com>
2015-08-31 21:31:22 -0700

    express installed serving static files

---
+++ public/scripts/app.js
@@ -0,0 +1,15 @@
+angular.module('pubCrawl', ['ngAutocomplete', 'ngMap'])
+
+  .controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+
+    $scope.start = '';
+    $scope.options1 = null;
+
+  }]);
+
+
+
+
+
+
+
