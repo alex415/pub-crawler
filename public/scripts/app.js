9a977c6ba1a4fcaad1e04682be1798a75c7aacee
Alex Chan <alex415@gmail.com>
2015-09-01 00:14:52 -0700

    edited search form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,20 @@
 angular.module('pubCrawl', ['ngAutocomplete', 'ngMap'])
 
-  .controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 
-    $scope.start = '';
-    $scope.options1 = null;
+  $scope.submit = function () {
+    var tag = $scope.autocomplete;
+    var url = 'https://api.foursquare.com/v2/venues/categories?client_id=0LQEK2QFONRMHNYOBLU4ZEMSGKGWAB5J51O4JB0DPYRNW41G&client_secret=JYZ2IHWEDEKK5A3HNQKO4ELARI55YOJP0LFOF1NFM3R3LY5Z&v=20150901';
+    $http.get(url)
+      .then(function (response) {
+        console.log(response);
+        console.log("hello");
+    });
+  };
 
-  }]);
+    $scope.autocomplete = '';
+    $scope.details = '';
+    $scope.options = '';
 
 
-
-
-
-
-
+}]); // end
