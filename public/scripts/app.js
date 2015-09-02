993fdcf109a5a35e96fab46859fafadc971ae773
Alex Chan <alex415@gmail.com>
2015-09-01 23:25:21 -0700

    switched to mapbox

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,6 @@
-angular.module('pubCrawl', ['ngAutocomplete', 'ngMap'])
+angular.module('pubCrawl', ['ngAutocomplete', 'ngMap', 'angular-mapbox']).run(function(mapboxService) {
+  mapboxService.init({ accessToken: 'pk.eyJ1IjoiYWxleDQxNSIsImEiOiJmNWM4YzNjMmVmYzQyNjk1N2U1NzE2ZDIzZTU1YmFjMiJ9.Dl4FXdWAWmXKWKA4jTwCeA' });
+})
 
 .controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 
@@ -8,9 +10,12 @@ angular.module('pubCrawl', ['ngAutocomplete', 'ngMap'])
     var url = 'https://api.foursquare.com/v2/venues/search?radius=1000&ll=' + geolocation + '&limit=10&client_id=0LQEK2QFONRMHNYOBLU4ZEMSGKGWAB5J51O4JB0DPYRNW41G&v=20150901&client_secret=JYZ2IHWEDEKK5A3HNQKO4ELARI55YOJP0LFOF1NFM3R3LY5Z&categoryId=4bf58dd8d48988d116941735';
     $http.get(url)
       .then(function (response) {
-        console.log(response);
+        $scope.bars = response.data.response;
+        // console.log(response);
     });
   };
+
+
 
 
 }]); // end
