a39edb3c6c0de3ded495c797be2e6ce6257a08dd
Alex Chan <alex415@gmail.com>
2015-09-02 00:24:45 -0700

    auth set up for mapbox

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,7 +6,6 @@ angular.module('pubCrawl', ['ngAutocomplete', 'ngMap', 'angular-mapbox']).run(fu
 
   $scope.submit = function () {
     var geolocation = $scope.details.geometry.location.toString().replace(/\s+/, '').slice(1, -1);
-    // console.log(geolocation);
     var url = 'https://api.foursquare.com/v2/venues/search?radius=1000&ll=' + geolocation + '&limit=10&client_id=0LQEK2QFONRMHNYOBLU4ZEMSGKGWAB5J51O4JB0DPYRNW41G&v=20150901&client_secret=JYZ2IHWEDEKK5A3HNQKO4ELARI55YOJP0LFOF1NFM3R3LY5Z&categoryId=4bf58dd8d48988d116941735';
     $http.get(url)
       .then(function (response) {
