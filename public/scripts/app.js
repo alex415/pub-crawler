14ea8e6aaec09f551f9ec6addac3d7733fdc3f74
Alex Chan <alex415@gmail.com>
2015-09-03 15:18:14 -0700

    switched from mapbox to google maps

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,22 @@
-angular.module('pubCrawl', ['ngAutocomplete', 'angular-mapbox']).run(function(mapboxService) {
-  mapboxService.init({ accessToken: 'pk.eyJ1IjoiYWxleDQxNSIsImEiOiJmNWM4YzNjMmVmYzQyNjk1N2U1NzE2ZDIzZTU1YmFjMiJ9.Dl4FXdWAWmXKWKA4jTwCeA' });
-})
+angular.module('pubCrawl', ['ngAutocomplete', 'ngMap'])
 
 .controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 
   $scope.submit = function () {
     var geolocation = $scope.details.geometry.location.toString().replace(/\s+/, '').slice(1, -1);
-    var url = 'https://api.foursquare.com/v2/venues/search?radius=1000&ll=' + geolocation + '&limit=10&client_id=0LQEK2QFONRMHNYOBLU4ZEMSGKGWAB5J51O4JB0DPYRNW41G&v=20150901&client_secret=JYZ2IHWEDEKK5A3HNQKO4ELARI55YOJP0LFOF1NFM3R3LY5Z&categoryId=4bf58dd8d48988d116941735';
+    var url = 'https://api.foursquare.com/v2/venues/explore?client_id=0LQEK2QFONRMHNYOBLU4ZEMSGKGWAB5J51O4JB0DPYRNW41G&client_secret=JYZ2IHWEDEKK5A3HNQKO4ELARI55YOJP0LFOF1NFM3R3LY5Z&v=20150901&ll=' + geolocation + '&query=drinks&limit=10&radius=1000';
     $http.get(url)
       .then(function (response) {
-        $scope.bars = response.data.response;
+        console.log(response.data.response.groups[0].items);
+        $scope.bars = response.data.response.groups[0].items;
     });
   };
 
+  $scope.maps = ['San Francisco'];
 
-
-  // https://api.foursquare.com/v2/venues/explore?ll=40.7,-74&client_id=0LQEK2QFONRMHNYOBLU4ZEMSGKGWAB5J51O4JB0DPYRNW41G&client_secret=JYZ2IHWEDEKK5A3HNQKO4ELARI55YOJP0LFOF1NFM3R3LY5Z&v=20150901&limit=10&section=drinks
+  $scope.addMap = function() {
+      $scope.maps.push($scope.newMap);
+  };
 
 
 
---
+++ public/scripts/maps_lib.js
@@ -0,0 +1,25 @@
+// When the window has finished loading create our google map below
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+  // Basic options for a simple Google Map
+  // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+  var mapOptions = {
+    // How zoomed in you want the map to start at (always required)
+    zoom: 11,
+
+    // The latitude and longitude to center the map (always required)
+    center: new google.maps.LatLng(37.774929, -122.419416),
+
+    // How you would like to style the map. 
+    // This is where you would paste any style found on Snazzy Maps.
+    styles: [{"featureType":"all","stylers":[{"saturation":-70},{"gamma":0.9}]}]
+  };
+
+  // Get the HTML DOM element that will contain your map.
+  // We are using a div with id="map" seen in the <body>
+  var mapElement = document.getElementById('map');
+
+  // Create the Google Map using our element and options defined above
+  var map = new google.maps.Map(mapElement, mapOptions);
+}
