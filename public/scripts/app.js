dd655f1019e1d6bb45a3769f3ad29e3666c3328a
Alex Chan <alex415@gmail.com>
2015-09-04 16:21:30 -0700

    set up instagram api

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,6 +5,7 @@ angular.module('pubCrawl', ['ngAutocomplete', 'ngMap'])
   var markers = [];
 
   $scope.submit = function () {
+
     // foursquare api call
     var geolocation = $scope.details.geometry.location.toString().replace(/\s+/, '').slice(1, -1);
     var url = 'https://api.foursquare.com/v2/venues/explore?client_id=0LQEK2QFONRMHNYOBLU4ZEMSGKGWAB5J51O4JB0DPYRNW41G&client_secret=JYZ2IHWEDEKK5A3HNQKO4ELARI55YOJP0LFOF1NFM3R3LY5Z&v=20150901&ll=' + geolocation + '&query=drinks&limit=10&radius=1000';
@@ -37,14 +38,13 @@ angular.module('pubCrawl', ['ngAutocomplete', 'ngMap'])
     }
   };
 
-  $scope.instagram = function () {
+  $scope.getInstagram = function (bar) {
     // instagram api call
-    var lat = '';
-    var lng = '';
-    var latlng = '';
-    var geolocation = $scope.details.geometry.location.toString().replace(/\s+/, '').slice(1, -1);
-    var url = 'https://api.instagram.com/v1/locations/search?lat=' + lat + '&lng=' + lng + '&client_id=58d7ef2ba19c481f84f6b8c3c2dbe895';
-    $http.get(url)
+    console.log(bar);
+    var lat = bar.latLng.G;
+    var lng = bar.latLng.K;
+    var url = 'https://api.instagram.com/v1/locations/search?lat=' + lat + '&lng=' + lng + '&client_id=58d7ef2ba19c481f84f6b8c3c2dbe895&callback=JSON_CALLBACK';
+    $http.jsonp(url)
       .then(function (response) {
         console.log(response);
         $scope.instagram = response;
