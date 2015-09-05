eee90dcfefb96443abc4d774e5aed92f343155b1
Alex Chan <alex415@gmail.com>
2015-09-05 01:20:04 -0700

    added circle and start icon to map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,6 +8,7 @@ angular.module('pubCrawl', ['ngAutocomplete', 'ngMap'])
 
     // foursquare api call
     var geolocation = $scope.details.geometry.location.toString().replace(/\s+/, '').slice(1, -1);
+    $scope.startLocation = $scope.details.geometry.location.toString().replace(/\s+/, '').slice(1, -1);
     var url = 'https://api.foursquare.com/v2/venues/explore?client_id=0LQEK2QFONRMHNYOBLU4ZEMSGKGWAB5J51O4JB0DPYRNW41G&client_secret=JYZ2IHWEDEKK5A3HNQKO4ELARI55YOJP0LFOF1NFM3R3LY5Z&v=20150901&ll=' + geolocation + '&query=drinks&limit=10&radius=1000';
     $http.get(url)
       .then(function (response) {
