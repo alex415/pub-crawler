12e4e8313a6f12a6874ef5c540e910ea9854d096
Alex Chan <alex415@gmail.com>
2015-09-05 22:02:06 -0700

    set up landing and about page

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/views/index.html');
 });
 
+// landing page route
+app.get('/landing', function (req, res) {
+  res.sendFile(__dirname + '/public/views/landing.html');
+});
+
+// landing page route
+app.get('/about', function (req, res) {
+  res.sendFile(__dirname + '/public/views/about.html');
+});
+
 // listen on port 3000
 app.listen(process.env.PORT || 3000, function () {
   console.log('server started on localhost:3000');
