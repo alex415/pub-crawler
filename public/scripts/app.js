4f3c91a0c4a12c000820ec08d5d49f7fa25ad69a
Alex Chan <alex415@gmail.com>
2015-09-07 21:12:22 -0700

    bug fix

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -42,9 +42,8 @@ angular.module('pubCrawl', ['ngAutocomplete', 'ngMap'])
   };
 
   $scope.getPictures = function (bar) {
-    // instagram api call
     var foursquareId = bar.venue.id;
-    console.log(bar.venue.id);
+    // console.log(bar.venue.id);
     var url = 'https://api.foursquare.com/v2/venues/' + foursquareId +'/photos?client_id=0LQEK2QFONRMHNYOBLU4ZEMSGKGWAB5J51O4JB0DPYRNW41G&client_secret=JYZ2IHWEDEKK5A3HNQKO4ELARI55YOJP0LFOF1NFM3R3LY5Z&v=20150901';
     $http.get(url)
       .then(function (response) {
