aec6b7a94ba5e31358fe6b99bf00b6a41d1ca3ce
Alex Chan <alex415@gmail.com>
2015-09-27 19:38:46 -0700

    dry

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -23,12 +23,9 @@ angular.module('pubCrawl', ['ngAutocomplete', 'ngMap', 'ngRoute'])
 
   $scope.submit = function () {
 
-    // starting icon location
-    var geolocation = $scope.details.geometry.location.toString().replace(/\s+/, '').slice(1, -1);
-
     // send user's location to foursquare API
     $scope.startLocation = $scope.details.geometry.location.toString().replace(/\s+/, '').slice(1, -1);
-    var url = 'https://api.foursquare.com/v2/venues/explore?client_id=0LQEK2QFONRMHNYOBLU4ZEMSGKGWAB5J51O4JB0DPYRNW41G&client_secret=JYZ2IHWEDEKK5A3HNQKO4ELARI55YOJP0LFOF1NFM3R3LY5Z&v=20150901&ll=' + geolocation + '&query=drinks&limit=10&radius=1500';
+    var url = 'https://api.foursquare.com/v2/venues/explore?client_id=0LQEK2QFONRMHNYOBLU4ZEMSGKGWAB5J51O4JB0DPYRNW41G&client_secret=JYZ2IHWEDEKK5A3HNQKO4ELARI55YOJP0LFOF1NFM3R3LY5Z&v=20150901&ll=' + $scope.startLocation + '&query=drinks&limit=10&radius=1500';
     $http.get(url)
       .then(function (response) {
         $scope.bars = response.data.response.groups[0].items;
